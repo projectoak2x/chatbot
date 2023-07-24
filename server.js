8f71ceb59ce0c2e0cc61bd9b688e3251f936aadf
Orlando Sarceda Jr <orlando@jiffiwebhelp.com>
2023-07-24 11:55:45 +0800

    initiate webhook

--- server.js
+++ server.js
@@ -18,6 +18,58 @@ const openai = new OpenAIApi(configuration);
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
 
+app.post('/webhook', async (req, res) => {
+  let body = req.body;
+  if (body.object === "page") {
+    // Returns a '200 OK' response to all requests
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+})
+
+// Add support for GET requests to our webhook
+app.get("/webhook", (req, res) => {
+  
+  // Parse the query params
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+  
+    // Check if a token and mode is in the query string of the request
+    if (mode && token) {
+      // Check the mode and token sent is correct
+      if (mode === "subscribe" && token === process.env.WEBHOOK_TOKEN) {
+        // Respond with the challenge token from the request
+        console.log("WEBHOOK_VERIFIED");
+        res.status(200).send(challenge);
+      } else {
+        // Respond with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);
+      }
+    }
+  });
+
+  // Verify that the callback came from Facebook.
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature-256"];
+
+  if (!signature) {
+    console.warn(`Couldn't find "x-hub-signature-256" in headers.`);
+  } else {
+    var elements = signature.split("=");
+    var signatureHash = elements[1];
+    var expectedHash = crypto
+      .createHmac("sha256", config.appSecret)
+      .update(buf)
+      .digest("hex");
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
+
 
 app.get('/chat', async (req, res) => {
   const q = req.query.q;
